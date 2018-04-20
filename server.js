e625c75a848594064a1edca4ee2330a59e47e287
Robert Rak <robertrak@hotmail.com>
2018-04-20 13:55:01 -0400

    Project completed

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const corsOptions = {
   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
+ // Access-Control-Allow-Origin: 'http://localhost:3000',
 };
 
 server.use(bodyParser.json());
-server.use(cors());
+//server.use(cors(corsOptions));
 
 routes(server);
 
